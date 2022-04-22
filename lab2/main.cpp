de124d24c70c233e1bf960692effb0b28ddc55b1
p1xel <dima.neumoin1234@gmail.com>
2022-04-22 22:00:31 +0300

    add main example

---
+++ lab2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include "Set.h"
+
+using namespace std;
+
+template <typename T>
+void printSet(Set<T> &set) {
+    for (auto iter = set.cbegin(); iter != set.cend(); ++iter)
+        cout << *iter << " ";
+
+    cout << endl;
+}
+template <typename T>
+void printReverseSet(Set<T> &set) {
+    for (auto iter = set.cend();; --iter) {
+        if (iter != set.cend()) cout << *iter << " ";
+        if (iter == set.cbegin()) break;
+    }
+
+    cout << endl;
+}
+
+template <typename T>
+void printSet(Set<T> &&set) {
+    printSet(set);
+}
+
+int main() {
+    Set<int> set1({1, 2, 3});
+    auto iter = set1.cend();
+    // iter++;
+    // iter++;
+    // iter--;
+    printReverseSet(set1);
+    // cout << *iter;
+
+    // Set<int> set2({2, 3});
+    // printSet(set2);
+
+    // printSet(set1 + set2);
+    // printSet(set1 - set2);
+    // auto iter = set1.cend();
+    // iter--;
+
+    return 0;
+}
